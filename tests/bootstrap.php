2d2e6f1842980b399d099c8b5e2a2ae74503b206
Rick Earley <rick@earleyholdings.com>
2014-06-02 15:46:16 -0400

    Updated bootstrap.php for Travis CI usage.

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+// Error Reporting
+error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+// Setup Autoloader
+$loader = require __DIR__."/../vendor/autoload.php";
+$loader->add('Ebay\\', '/var/www/projects/ebay/src');
+echo __DIR__;
